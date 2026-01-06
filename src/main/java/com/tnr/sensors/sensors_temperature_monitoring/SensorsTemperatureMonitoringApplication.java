8f3db5448753b066e0a2ca07c7924418bdea7a4f
Tony Araujo <tnr.araujo@gmail.com>
2026-01-06 19:00:45 -0300

    First commit

---
+++ src/main/java/com/tnr/sensors/sensors_temperature_monitoring/SensorsTemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.tnr.sensors.sensors_temperature_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorsTemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorsTemperatureMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnr/sensors/sensors_temperature_monitoring/SensorsTemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnr.sensors.sensors_temperature_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsTemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
